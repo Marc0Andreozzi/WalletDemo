aceceb07d626e601c51478b8d158d60c0beaac44
Marco <m.andreozziaziendavisibile.it>
2022-11-07 16:38:48 +0100

    Aggiunta enum TipoTransazione

---
+++ src/main/java/com/everestinnovation/walletdemo/repository/bean/TipoTransazione.java
@@ -0,0 +1,40 @@
+package com.everestinnovation.walletdemo.repository.bean;
+
+import java.util.Objects;
+
+public enum TipoTransazione {
+
+    IN_ENTRATA(1, "In Entrata"),
+    IN_USCITA(2,"In Uscita"),
+    TRASFERIMENTO_A(3, "Trasferimento a"),
+    TRASFERIMENTO_DA(4, "Trasferimetno da");
+
+    private final int idOp;
+
+    private final String tipoOp;
+
+    TipoTransazione(int idOp, String tipoOp) {
+        this.idOp = idOp;
+        this.tipoOp = tipoOp;
+    }
+
+    public static TipoTransazione ofIdOp(int idOp){
+        for(TipoTransazione value : TipoTransazione.values()){
+            if(idOp == value.idOp){
+                return value;
+            }
+        }
+        throw new EnumConstantNotPresentException(TipoUtenza.class, String.valueOf(idOp));
+    }
+
+    public static TipoTransazione ofTipoOp(String tipoOp){
+        for(TipoTransazione value : TipoTransazione.values()){
+            if (Objects.equals(tipoOp, value.tipoOp)){
+                return value;
+            }
+        }
+        throw new EnumConstantNotPresentException(TipoUtenza.class, String.valueOf(tipoOp));
+    }
+
+}
+
