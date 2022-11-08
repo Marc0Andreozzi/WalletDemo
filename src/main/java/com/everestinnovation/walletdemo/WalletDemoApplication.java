a4fd8ef1cec98edebddfb01e340e690fee4ac32b
Marco <m.andreozziaziendavisibile.it>
2022-11-08 22:53:36 +0100

    Aggiunta exclude

--- src/main/java/com/everestinnovation/walletdemo/WalletDemoApplication.java
+++ src/main/java/com/everestinnovation/walletdemo/WalletDemoApplication.java
@@ -2,8 +2,9 @@ package com.everestinnovation.walletdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class })
 public class WalletDemoApplication {
 
     public static void main(String[] args) {
