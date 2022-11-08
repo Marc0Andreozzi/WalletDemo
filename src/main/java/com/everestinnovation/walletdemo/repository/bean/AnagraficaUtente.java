cf4602a6c951be3cbde33ec167864a5eb90523a4
Marco <m.andreozziaziendavisibile.it>
2022-11-08 11:43:43 +0100

    Modifiche campi tabelle

--- src/main/java/com/everestinnovation/walletdemo/repository/bean/AnagraficaUtente.java
+++ src/main/java/com/everestinnovation/walletdemo/repository/bean/AnagraficaUtente.java
@@ -2,10 +2,9 @@ package com.everestinnovation.walletdemo.repository.bean;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -17,15 +16,47 @@ public class AnagraficaUtente {
 
     private int idUser;
 
+    @Column(name = "nome")
+    @Size(min = 3, max = 30)
     private String nome;
+
+    @Column(name = "cognome")
+    @Size(min = 3, max = 30)
     private String cognome;
+
+    @Column(name = "codice_fiscale")
+    @Size(min = 16)
     private String codiceFiscale;
+
+    @Column(name = "indirizzo")
+    @Size(min = 20, max = 50)
     private String indirizzo;
+
+    @Column(name = "provincia")
+    @Size(min = 2)
     private String provincia;
+
+    @Column(name = "comune")
+    @Size(min = 4, max = 10)
     private String comune;
+
+    @Column(name = "cap")
+    @Size(min = 5, max = 8 )
     private String cap;
+
+    @Column(name = "paese")
+    @Size(min = 5, max = 20)
     private String paese;
+
+    @Column(name = "nato_a")
+    @Size(min = 5, max = 30)
     private String natoA;
+
+    @Column(name = "data_di_nascita")
+    @Size(min = 8, max = 10)
     private String dataDiNascita;
+
+    @Column(name = "numero di telefono")
+    @Pattern(regexp = "\\(\\+[0-9]{1,5}\\)[0-9]{3,15}", message = "Formato numero non valido!")
     private int numeroDiTelefono;
 }
--- src/main/java/com/everestinnovation/walletdemo/repository/bean/User.java
+++ src/main/java/com/everestinnovation/walletdemo/repository/bean/User.java
@@ -3,6 +3,9 @@ package com.everestinnovation.walletdemo.repository.bean;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,15 +16,30 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
+    @Size(max = 254, message = "Email può contenere fino a 254 caratteri")
+    @Pattern(regexp = "[^@]+@[^\\.]+\\..+", message = "Formato Email non valido")
     private String email;
 
-    @Column(unique = true)
+    @Column(name = "username", unique = true, nullable = false)
+    @Size(min = 3, max = 30, message = "Username deve contenere un minimo di 3 caratteri ed un massimo di 30")
+    @Pattern(regexp = "[\\w]+", message = "Username può contenere lettere maiuscole, underscore e numeri")
     private String username;
 
+    @NotNull
+    @Column(name = "password")
+    @Size(min = 6, max = 8, message = "La password deve contenere minimo 6 caratteri, massimo 8 caratteri")
     private String password;
 
+    @NotNull
+    @Column(name = "conferma_password")
+    @Size(min = 6, max = 8)
+    private String confermaPassword;
+
+    @NotNull
     private String tipoUtenza;
+
+    @NotNull
     private String tipoWallet;
 
 }
