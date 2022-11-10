56781baf960430b1bbc6d3bd318539490dc8ef4b
Marco <m.andreozziaziendavisibile.it>
2022-11-10 09:49:42 +0100

    Modificata interfaccia di Repository estesa

--- src/main/java/com/everestinnovation/walletdemo/repository/UserRepository.java
+++ src/main/java/com/everestinnovation/walletdemo/repository/UserRepository.java
@@ -2,12 +2,17 @@ package com.everestinnovation.walletdemo.repository;
 
 
 import com.everestinnovation.walletdemo.repository.bean.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User save(User user);
 
-    public void deleteById(int id);
+    public void deleteById(long id);
+
+
 
 }
--- src/main/java/com/everestinnovation/walletdemo/repository/WalletRepository.java
+++ src/main/java/com/everestinnovation/walletdemo/repository/WalletRepository.java
@@ -1,13 +1,14 @@
 package com.everestinnovation.walletdemo.repository;
 
-import com.everestinnovation.walletdemo.repository.bean.User;
-import com.everestinnovation.walletdemo.repository.bean.Wallet;
-import org.springframework.data.repository.CrudRepository;
 
-public interface WalletRepository extends CrudRepository<Wallet, Integer> {
+import com.everestinnovation.walletdemo.repository.bean.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
     Wallet save(Wallet wallet);
 
-    public void deleteById(int id);
+    public void deleteById(long id);
 
 }
