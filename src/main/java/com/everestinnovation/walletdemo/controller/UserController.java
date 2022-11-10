6d7df4f572d729ec9ddd92c442004daa3861c405
Marco <m.andreozziaziendavisibile.it>
2022-11-10 18:45:53 +0100

    Modificati metodi post,get e delete User

--- src/main/java/com/everestinnovation/walletdemo/controller/UserController.java
+++ src/main/java/com/everestinnovation/walletdemo/controller/UserController.java
@@ -3,11 +3,14 @@ package com.everestinnovation.walletdemo.controller;
 import com.everestinnovation.walletdemo.repository.bean.User;
 import com.everestinnovation.walletdemo.repository.bean.Wallet;
 import com.everestinnovation.walletdemo.service.UserService;
+import com.everestinnovation.walletdemo.service.ValidationErrorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,6 +22,8 @@ import javax.validation.Valid;
 public class UserController {
 
     @Autowired
+    private ValidationErrorService validationErrorService;
+    @Autowired
     private UserService userService;
 
     public UserController (UserService userService) { this.userService=userService; }
@@ -27,30 +32,25 @@ public class UserController {
 
 
     @PostMapping
-    @ResponseStatus(value = HttpStatus.CREATED)
-    public void createUser (
-            @Valid @RequestBody User user,
-            @Valid @RequestBody Wallet wallet){
-
-        log.info("Richiesta ricevuta per creare un nuovo Utente.", LogLevel.INFO);
-        log.info(user.toString());
-        log.info(wallet.toString());
-
-        userService.saveUserAndWallet(user,wallet);
-
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
+        ResponseEntity errors = validationErrorService.validate(result);
+        if(errors != null) return errors;
+        log.info("Richiesta ricevuta per creare un nuovo Wallet.", LogLevel.INFO);
+        User userSaved = userService.createUser(user);
+        return new ResponseEntity<User>(userSaved,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(User user){
-
-        log.info("Richiesta ricevuta per cancellare un Utente.", LogLevel.INFO);
-        String userdId = String.valueOf(user.getId());
-        log.info("Id: " + userdId);
-
-        userService.deleteById(user);
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id){
+        return new ResponseEntity<>(userService.getById(id),HttpStatus.OK);
+    }
 
     @GetMapping("/list")
     public Iterable<User> list(){
