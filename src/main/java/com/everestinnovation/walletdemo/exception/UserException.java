2a79031d3cb42b23e700f62d1f674a8d7a5f71f6
Marco <m.andreozziaziendavisibile.it>
2022-11-10 18:39:10 +0100

    Aggiunta classe UserException

---
+++ src/main/java/com/everestinnovation/walletdemo/exception/UserException.java
@@ -0,0 +1,9 @@
+package com.everestinnovation.walletdemo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserException extends RuntimeException{
+    public UserException(String message) { super(message);}
+}
