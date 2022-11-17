086643ad7c4a1a455dbac5968e2db1d7fc6b07b4
Marco <m.andreozziaziendavisibile.it>
2022-11-17 16:53:44 +0100

    Rename repository

--- src/main/java/com/everestinnovation/walletdemo/repository/CredenzialiRepository.java
+++ src/main/java/com/everestinnovation/walletdemo/repository/CredenzialiRepository.java
@@ -5,7 +5,7 @@ import com.everestinnovation.walletdemo.repository.bean.Credenziali;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<Credenziali, Long> {
+public interface CredenzialiRepository extends JpaRepository<Credenziali, Long> {
 
 
 
