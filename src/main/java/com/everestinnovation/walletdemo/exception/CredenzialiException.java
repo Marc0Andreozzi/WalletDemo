d0117e279f8db2cbf3775d6a2680dd1461d27e02
Marco <m.andreozziaziendavisibile.it>
2022-11-17 16:54:22 +0100

    rinominata classe exception

--- src/main/java/com/everestinnovation/walletdemo/exception/CredenzialiException.java
+++ src/main/java/com/everestinnovation/walletdemo/exception/CredenzialiException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UserException extends RuntimeException{
-    public UserException(String message) { super(message);}
+public class CredenzialiException extends RuntimeException{
+    public CredenzialiException(String message) { super(message);}
 }
